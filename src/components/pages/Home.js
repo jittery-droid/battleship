013e2360645a039789fd78752da73a730e4ec311
jittery-droid <jittery.dev@gmail.com>
2020-11-15 00:25:23 -0600

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  const gfonts = require('google-fonts-complete')
+  console.log(gfonts)
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Battleship Bonanza</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home;
